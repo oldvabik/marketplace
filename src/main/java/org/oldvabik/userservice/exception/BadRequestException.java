52ce03c5fe78111d2140a275b09acc28787ce988
Maksim Vabishchevich <oldvabik@gmail.com>
2025-11-07 01:36:56 +0300

    remove files

--- src/main/java/org/oldvabik/userservice/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package org.oldvabik.userservice.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
